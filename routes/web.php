e32f157b71ab3bf456b9f2f1d5510ec8b4222abd
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-10 10:37:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,3 @@ use App\Http\Controllers\KeranjangController;
 Route::get('/', function () {
     return redirect('/admin');
 });
-
-Route::get('/api/getKeranjang', [KeranjangController::class, 'get']);
-Route::get('/api/addKeranjang', [KeranjangController::class, 'add']);
-Route::get('/api/removeKeranjang', [KeranjangController::class, 'remove']);
