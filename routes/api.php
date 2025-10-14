a12426d1bf599126d7c5d956ef8474ee45ebdafb
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-14 16:04:50 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,12 @@ use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\KeranjangController;
 use App\Http\Controllers\TrnAbsenController;
 
-// Resource routes
+// Rute untuk login dan register (tanpa autentikasi)
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/register', [LoginController::class, 'register']);
+Route::post('isValidLogin', [LoginController::class, 'isValidLogin']);
+
+// Rute untuk semua resource yang tidak memerlukan autentikasi
 Route::apiResource('barang', MstBarangController::class);
 Route::apiResource('departemen', MstDepartemenController::class);
 Route::apiResource('kota', MstKotaController::class);
@@ -28,30 +33,35 @@ Route::apiResource('kecamatan', MstKecamatanController::class);
 Route::apiResource('kelurahan', MstKelurahanController::class);
 Route::apiResource('karyawan', MstKaryawanController::class);
 Route::apiResource('satuan', MstSatuanController::class);
-//Route::apiResource('absen', TrnAbsenController::class);
 
-Route::get('searchBarang', [MstBarangController::class, 'searchBarang']);
+// Rute absensi tanpa autentikasi
+Route::get('absen', [TrnAbsenController::class, 'index']);
+Route::post('absen', [TrnAbsenController::class, 'store']);
+Route::get('absen/show', [TrnAbsenController::class, 'show']);
+Route::put('absen/update', [TrnAbsenController::class, 'update']);
+Route::delete('absen/delete', [TrnAbsenController::class, 'destroy']);
 Route::get('absengetkaryawan', [TrnAbsenController::class, 'getByIdKaryawan']);
 Route::get('checkAbsen', [TrnAbsenController::class, 'checkAbsen']);
 
+// Rute keranjang tanpa autentikasi
 Route::get('getKeranjang', [KeranjangController::class, 'get']);
 Route::post('addKeranjang', [KeranjangController::class, 'add']);
 Route::delete('removeKeranjang', [KeranjangController::class, 'remove']);
 
+// Rute sales order tanpa autentikasi
 Route::get('sales-order-header', [TrnSalesOrderHeaderController::class, 'index']);
 Route::post('sales-order-header', [TrnSalesOrderHeaderController::class, 'store']);
-
 Route::get('sales-order-header/show', [TrnSalesOrderHeaderController::class, 'show']);
 Route::put('sales-order-header/update', [TrnSalesOrderHeaderController::class, 'update']);
 Route::delete('sales-order-header/delete', [TrnSalesOrderHeaderController::class, 'destroy']);
 
 Route::get('sales-order-detail', [TrnSalesOrderDetailController::class, 'index']);
 Route::post('sales-order-detail', [TrnSalesOrderDetailController::class, 'store']);
-
 Route::get('sales-order-detail/show', [TrnSalesOrderDetailController::class, 'show']);
 Route::put('sales-order-detail/update', [TrnSalesOrderDetailController::class, 'update']);
 Route::delete('sales-order-detail/delete', [TrnSalesOrderDetailController::class, 'destroy']);
 
+// Rute customer rute tanpa autentikasi
 Route::get('customer-rute', [MstCustomerRuteController::class, 'index']);
 Route::get('customer-rute-by-id', [MstCustomerRuteController::class, 'getById']);
 Route::post('customer-rute', [MstCustomerRuteController::class, 'store']);
@@ -59,25 +69,5 @@ Route::get('customer-rute/show', [MstCustomerRuteController::class, 'show']);
 Route::put('customer-rute/update', [MstCustomerRuteController::class, 'update']);
 Route::delete('customer-rute/delete', [MstCustomerRuteController::class, 'destroy']);
 
-
-Route::get('absen', [TrnAbsenController::class, 'index']);
-Route::post('absen', [TrnAbsenController::class, 'store']);
-Route::get('absen/show', [TrnAbsenController::class, 'show']);
-Route::put('absen/update', [TrnAbsenController::class, 'update']);
-Route::delete('absen/delete', [TrnAbsenController::class, 'destroy']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('isValidLogin', [LoginController::class, 'isValidLogin']);
-Route::post('/register', [LoginController::class, 'register']);
-Route::post('/login', [LoginController::class, 'login']);
-
-// Route::middleware('auth:api')->group(function () {
-Route::get('/profile', [LoginController::class, 'profile']);
-Route::post('/logout', [LoginController::class, 'logout']);
-// });
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+// Rute searchBarang tanpa autentikasi
+Route::get('searchBarang', [MstBarangController::class, 'searchBarang']);
