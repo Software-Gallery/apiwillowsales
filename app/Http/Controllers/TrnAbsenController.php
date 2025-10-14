c509ac8fb592392058dc060c0586c4ef2c83506d
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-14 15:37:51 +0700

    Update TrnAbsenController.php

--- app/Http/Controllers/TrnAbsenController.php
+++ app/Http/Controllers/TrnAbsenController.php
@@ -55,23 +55,23 @@ class TrnAbsenController extends Controller
 
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'id_karyawan' => 'required|integer',
-            'id_customer' => 'required|integer',
-            'id_departemen' => 'required|integer',
-            'tgl' => 'required|date',
-            'jam_masuk' => 'required|date_format:H:i',
-            'latitude' => 'required|numeric',
-            'longitude' => 'required|numeric',
-            'keterangan' => 'nullable|string|max:255',
-            'alamat' => 'nullable|string|max:255',
-        ]);
+        // $validated = $request->validate([
+        //     'id_karyawan' => 'required|integer',
+        //     'id_customer' => 'required|integer',
+        //     'id_departemen' => 'required|integer',
+        //     'tgl' => 'required|date',
+        //     'jam_masuk' => 'required|date_format:H:i',
+        //     'latitude' => 'required|numeric',
+        //     'longitude' => 'required|numeric',
+        //     'keterangan' => 'nullable|string|max:255',
+        //     'alamat' => 'nullable|string|max:255',
+        // ]);
 
-        $absen = TrnAbsen::create($validated);
+        // $absen = TrnAbsen::create($validated);
 
         return response()->json([
             'message' => 'Absen berhasil disimpan!',
-            'data' => $absen
+            'data' => 'Test'
         ], 201);
     }
 
