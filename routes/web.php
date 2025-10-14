23db5253aa69db5248b3d64f4d8f894506288c7b
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-14 15:33:07 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\KeranjangController;
 Route::get('/', function () {
     return redirect('/admin');
 });
+
+Route::get('/api/insertAbsen', [TrnAbsenController::class, 'store']);
