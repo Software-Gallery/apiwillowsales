e73f974112c72ee5f7e437aa734446a89bca99e9
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-14 15:34:23 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KeranjangController;
+use App\Http\Controllers\TrnAbsenController;
 
 Route::get('/', function () {
     return redirect('/admin');
