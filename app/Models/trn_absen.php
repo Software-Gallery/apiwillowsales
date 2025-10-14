e90d747b0709f6239dee8ec47a12865d536ab75c
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-14 16:21:53 +0700

    Update trn_absen.php

--- app/Models/trn_absen.php
+++ app/Models/trn_absen.php
@@ -4,20 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class trn_absen extends Model
+class TrnAbsen extends Model
 {
     protected $table = 'trn_absen';
     protected $primaryKey = 'id_absen';
     protected $fillable = [
-        "id_karyawan ",
-        "id_customer ",
-        "kode_sales_order ",
-        "tgl ",
-        "jam_masuk ",
-        "jam_keluar ",
-        "latitude ",
-        "longitude ",
-        "keterangan ",
+        "id_karyawan",
+        "id_customer",
+        "kode_sales_order",
+        "tgl",
+        "jam_masuk",
+        "jam_keluar",
+        "latitude",
+        "longitude",
+        "keterangan",
         "alamat"
-    ];    
+    ];
 }
