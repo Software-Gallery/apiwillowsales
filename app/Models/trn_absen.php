b58e088cec27783bb91fd1bf7718a7efdd27773f
Software-Gallery <softwaregallery.sby@gmail.com>
2025-10-14 10:34:35 +0700

    update model -mr trn_absen

---
+++ app/Models/trn_absen.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class trn_absen extends Model
+{
+    protected $table = 'trn_absen';
+    protected $primaryKey = 'id_absen';
+    protected $fillable = [
+        "id_karyawan ",
+        "id_customer ",
+        "kode_sales_order ",
+        "tgl ",
+        "jam_masuk ",
+        "jam_keluar ",
+        "latitude ",
+        "longitude ",
+        "keterangan ",
+        "alamat"
+    ];    
+}
---
+++ database/migrations/2025_10_14_033128_create_trn_absens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trn_absen', function (Blueprint $table) {
+            $table->id('id_absen'); 
+            $table->foreignId('id_karyawan')->constrained('karyawan'); 
+            $table->foreignId('id_customer')->constrained('customers'); 
+            $table->string('kode_sales_order'); 
+            $table->date('tgl'); 
+            $table->time('jam_masuk'); 
+            $table->time('jam_keluar'); 
+            $table->double('latitude'); 
+            $table->double('longitude'); 
+            $table->string('keterangan')->nullable(); 
+            $table->string('alamat')->nullable(); 
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trn_absens');
+    }
+};
