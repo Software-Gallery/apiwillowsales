126a5fe84278da9ea4ff4fdcb209f136382339ff
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-15 16:08:38 +0700

    Update TrnSalesOrderHeaderController.php

--- app/Http/Controllers/TrnSalesOrderHeaderController.php
+++ app/Http/Controllers/TrnSalesOrderHeaderController.php
@@ -67,7 +67,7 @@ class TrnSalesOrderHeaderController extends Controller
         }                
 
         $validated['kode_sales_order'] = $year . $month . $nextNumber;
-        $validated['tgl_sales_order'] = ;
+        $validated['tgl_sales_order'] = now()->setTimezone('Asia/Jakarta')->format('Y-m-d'));
         
         $order = trn_sales_order_header::create($validated);
         return response()->json([
