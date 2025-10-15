1ca13fede123fbd39320127050cc19c2c86290f5
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-15 08:16:21 +0700

    Update TrnAbsenController.php

--- app/Http/Controllers/TrnAbsenController.php
+++ app/Http/Controllers/TrnAbsenController.php
@@ -55,27 +55,26 @@ class TrnAbsenController extends Controller
 
 public function store(Request $request)
 {
-    // Validasi data input
-    $validated = $request->validate([
-        'id_karyawan' => 'required|integer',
-        'id_customer' => 'required|integer',
-        'id_departemen' => 'required|integer',
-        'tgl' => 'required|date',
-        'jam_masuk' => 'required|date_format:H:i',
-        'latitude' => 'required|numeric',
-        'longitude' => 'required|numeric',
-        'keterangan' => 'nullable|string|max:255',
-        'alamat' => 'nullable|string|max:255',
-    ]);
-
-    // Simpan data absensi ke database
-    $absen = trn_absen::create($validated);
-
-    // Kembalikan response JSON setelah berhasil menyimpan data
+    // $validated = $request->validate([
+    //     'id_karyawan' => 'required|integer',
+    //     'id_customer' => 'required|integer',
+    //     'id_departemen' => 'required|integer',
+    //     'tgl' => 'required|date',
+    //     'jam_masuk' => 'required|date_format:H:i',
+    //     'latitude' => 'required|numeric',
+    //     'longitude' => 'required|numeric',
+    //     'keterangan' => 'nullable|string|max:255',
+    //     'alamat' => 'nullable|string|max:255',
+    // ]);
+    // $absen = trn_absen::create($validated);
+    // return response()->json([
+    //     'message' => 'Absen berhasil disimpan!',
+    //     'data' => $absen,
+    // ], 201);
     return response()->json([
-        'message' => 'Absen berhasil disimpan!',
-        'data' => $absen, // Menampilkan data yang baru disimpan
-    ], 201); // HTTP status code 201 untuk "Created"
+        'message' => 'Coba test pesan',
+        'data' => $request,
+    ], 200);    
 }
 
 
