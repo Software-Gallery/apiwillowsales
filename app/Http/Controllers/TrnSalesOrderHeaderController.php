4ded6bbb09bc9b4a391c9d0981c7065fb12a9d84
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-15 17:01:18 +0700

    Update TrnSalesOrderHeaderController.php

--- app/Http/Controllers/TrnSalesOrderHeaderController.php
+++ app/Http/Controllers/TrnSalesOrderHeaderController.php
@@ -81,7 +81,7 @@ class TrnSalesOrderHeaderController extends Controller
         // Loop untuk setiap item di keranjang dan simpan ke trn_sales_order_detail
         foreach ($keranjangs as $keranjang) {
             // Hitung harga, diskon, dan subtotal jika perlu. Misalnya, kita asumsikan harga barang tersedia
-            $barang = mst_barang::find($keranjang->id_barang);
+            $barang = mst_barang::where('id_barang', $keranjang->id_barang);
             $harga = $barang ? $barang->harga : 0;  // Jika barang tidak ditemukan, harga default 0            
             // $harga = 1000;  // Ini adalah contoh, sesuaikan dengan logika harga yang sesuai
             $disc_cash = 0;  // Diskon tunai, misalnya 0
