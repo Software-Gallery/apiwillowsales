21be40ff90cb762ada2041a35004663281eb9dfc
Software-Gallery <149457808+Software-Gallery@users.noreply.github.com>
2025-10-16 11:19:04 +0700

    Update TrnAbsenController.php

--- app/Http/Controllers/TrnAbsenController.php
+++ app/Http/Controllers/TrnAbsenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\trn_absen;
+use Illuminate\Support\Facades\Storage;
 
 class TrnAbsenController extends Controller
 {
